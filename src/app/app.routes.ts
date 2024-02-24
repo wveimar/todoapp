c93d2efccd13acd616eebd3f80c09e13faa4abb8
wveimar <wveimar.mamian@gmail.com>
2024-02-24 11:47:54 -0500

    wp: configuration tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/todo',
+  },
+
+  {
+    path: 'todo',
+    component: TodoComponent,
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#871cf8",
+        "background-100": "#1A1A1A",
+        "background-200": "#292929",
+        "background-300": "#404040",
+        "background-400": "#5B5B5B",
+      }
+    },
   },
   plugins: [],
 }
