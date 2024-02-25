c8fd402d197983f94584dd4aaa5d258069a61939
wveimar <wveimar.mamian@gmail.com>
2024-02-24 20:52:39 -0500

    Implementation of control structures (for and if) to iterate through and process a JSON object.

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FilterType, TodoModel } from '../../models/todo';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  todoList = signal<TodoModel[]>([
+    {
+      id: 1,
+      title: 'buy milk',
+      completed: false,
+      editing: false,
+    },
+    {
+      id: 2,
+      title: 'buy bread',
+      completed: false,
+      editing: false,
+    },
+    {
+      id: 3,
+      title: 'buy cheese',
+      completed: false,
+      editing: false,
+    },
+  ]);
 
+  filter = signal<FilterType>("all");
+
+  changeFilter(filterString: FilterType){
+  this.filter.set(filterString);
+  }
 }
