14de55d4507da0540bc04eebd00a2479897c80ca
wveimar <wveimar.mamian@gmail.com>
2024-02-24 22:17:43 -0500

    Implement functionality for adding todos (addTodo) and toggling todo completion status (ToggleTodo).

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FilterType, TodoModel } from '../../models/todo';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrl: './todo.component.css',
 })
 export class TodoComponent {
   todoList = signal<TodoModel[]>([
@@ -26,14 +27,47 @@ export class TodoComponent {
     {
       id: 3,
       title: 'buy cheese',
-      completed: false,
+      completed: true,
       editing: false,
     },
   ]);
 
-  filter = signal<FilterType>("all");
+  filter = signal<FilterType>('all');
 
-  changeFilter(filterString: FilterType){
-  this.filter.set(filterString);
+  newTodo = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(3)],
+  });
+
+  changeFilter(filterString: FilterType) {
+    this.filter.set(filterString);
+  }
+
+  addTodo() {
+    const newTodoTitle = this.newTodo.value.trim();
+    if (this.newTodo.valid && newTodoTitle !== '') {
+      this.todoList.update((prev_todos) => {
+        return [
+          ...prev_todos,
+          { id: prev_todos.length + 1, title: newTodoTitle, completed: false },
+        ];
+      });
+      this.newTodo.reset();
+    } else {
+      this.newTodo.reset();
+    }
+  }
+
+  toggleTodo(todoId: number) {
+    return this.todoList.update((prev_todos) =>
+      prev_todos.map((todo) => {
+        return todo.id === todoId
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }:
+             todo;
+      })
+    );
   }
 }
